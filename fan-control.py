1af187d8b7bd5a497dad408db504970fee341caf
Luke Seale <19484084+lbseale@users.noreply.github.com>
2019-12-17 06:46:02 -0800

    Rewrite to use a lookup function - Lookup the temp in the temp_list if it might increase - Lookup the temp in the hysteresis list if it might decrease - Set the fan setting accordingly

--- fan-control.py
+++ fan-control.py
@@ -10,29 +10,48 @@ class FanController:
         self.fan_on = False
         self.current_fan_level = 0
 
+        # Define hysteresis trip points as the ordinary trip points minus the hysteresis
+        hyst_trip_temps = []
+        for temp in self.trip_temps:
+            if(temp == 0):
+                hyst_trip_temps.append(0)
+            else:
+                hyst_trip_temps.append(temp - self.hysteresis)
+        self.hyst_trip_temps = hyst_trip_temps
+
     def set_speed(self, current_temp):
 
-        fan_on = (self.current_fan_level > 0)
-        new_fan_level = -1
+        current_trip_temp = self.trip_temps[self.current_fan_level]
 
-        # If the fan is on, check if current_temp is below hysteresis of current level (or one below), and we need to slow down
-        if fan_on:
-            for i in range(1, (self.current_fan_level + 1)):
-                off_temp = (self.trip_temps[i] - self.hysteresis)
-                if(current_temp < off_temp):
-                    new_fan_level = (i - 1)
-                    break
+        # Look up the current temp in the trip_temp list
+        # Use the ordinary list if the level might increase
+        # Use the hysteresis list if the level mimght decrease
+        if(current_temp >= current_trip_temp):
+            new_fan_level = self._lookup(self.trip_temps, current_temp)
+        elif(current_temp < current_trip_temp):
+            new_fan_level = self._lookup(self.hyst_trip_temps, current_temp)
 
-        # Look to see if current_temp is above any trip point above the current level, and we need to speed up
-        # Loop backwards through trip_temps, up to the current one
-        if(new_fan_level != -1):
-            for i in range((len(self.trip_temps) - 1), (self.current_fan_level - 1), -1):
-                if(current_temp > self.trip_temps[i]):
-                    new_fan_level = i
-                    break
+        self.current_fan_level = new_fan_level
+        return self.trip_speeds[new_fan_level]
 
-        if(new_fan_level != -1):
-            self.current_fan_level = new_fan_level
-            return self.trip_speeds[new_fan_level]
-        else:
-            return self.trip_speeds[self.current_fan_level]
+    # Look up a temperature in the temp_list, being smart about greater than/less than
+    def _lookup(self, temp_list, temp):
+        list_end = len(temp_list)-1
+        found_index = -1
+        for i in range(len(temp_list)):
+            if(i == 0 and temp < temp_list[i]):
+                found_index = i
+            elif(i == list_end and temp >= temp_list[i]):
+                found_index = i
+            elif(i < list_end and temp >= temp_list[i] and temp < temp_list[i+1]):
+                found_index = i
+        return found_index
+
+def debug1():
+    fc = FanController()
+    test_temps = [45000, 55000, 61000, 60000, 59000, 58000, 57000, 72000, 75000, 80000, 79000, 77000, 59000, 57000]
+
+    for tt in test_temps:
+        print('test temp: ' + str(tt) + ' set speed: ' + str(fc.set_speed(tt)))
+
+debug1()
