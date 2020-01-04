af6d588ad38050b5a74b971f6a6983c0fc78a011
lbseale <19484084+lbseale@users.noreply.github.com>
2020-01-03 16:51:23 -0800

    Add line for license in setup.py
    - Chose the MIT license, since it's simple

--- setup.py
+++ setup.py
@@ -20,6 +20,7 @@ setup(
         'Development Status :: 3 - Alpha',
         'Intended Audience :: End Users/Desktop'
         'Topic :: System :: Hardware',
+        'License :: OSI Approved :: MIT License',
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
